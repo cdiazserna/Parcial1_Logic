3462f331ca29d874b0b5396a8d776e3bc657bacd
Andy Morales <andyemmejia@gmail.com>
2023-03-09 18:48:56 -0500

    Cambio 1

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -2,11 +2,45 @@
 
 namespace Punto1
 {
-    internal class Program
+    public class Punto1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double lado, b, altura, areaCuad, areaTrian, areaRectan, areaCirc;
+            int option;
+
+            Console.WriteLine("******");
+            Console.WriteLine("*Menú*");
+            Console.WriteLine("******");
+            Console.WriteLine("1. Calcular área de un cuadrado ");
+            Console.WriteLine("2. Calcular área de un rectángulo");
+            Console.WriteLine("3. Calcular área de un triángulo");
+            Console.WriteLine("4. Calcular área de un círculo");
+
+            option = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Por favor ingrese el lado");
+            lado = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Por favor ingrese el lado");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Por favor ingrese el lado");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+
+
+            switch (option)
+            {
+                case 1:
+
+                    areaCuad = Math.Pow(lado, 2);
+
+            }
+
+
+
+
         }
     }
 }
