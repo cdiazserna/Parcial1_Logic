1613418f1cc2e24d2763785abbc24b54b96e7990
jjtafur <jjtafur22@gmail.com>
2023-03-09 18:27:35 -0500

    Declaración variables y opciones

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -9,55 +9,14 @@ namespace Punto1
             int opcion;
             double valor1, valor2, area;
 
-            do
-            {
-                Console.WriteLine("Seleccione una opción:");
-                Console.WriteLine("1. Calcular área de un cuadrado");
-                Console.WriteLine("2. Calcular área de un rectángulo");
-                Console.WriteLine("3. Calcular área de un triángulo");
-                Console.WriteLine("4. Calcular área de un círculo");
-                Console.WriteLine("0. Salir");
 
-                opcion = int.Parse(Console.ReadLine());
+            Console.WriteLine("Seleccione una opción:");
+            Console.WriteLine("1. Calcular área de un cuadrado");
+            Console.WriteLine("2. Calcular área de un rectángulo");
+            Console.WriteLine("3. Calcular área de un triángulo");
+            Console.WriteLine("4. Calcular área de un círculo");
+            Console.WriteLine("0. Salir");
 
-                switch (opcion)
-                {
-                    case 1:
-                        Console.WriteLine("Ingrese el valor del lado del cuadrado:");
-                        valor1 = double.Parse(Console.ReadLine());
-                        area = valor1 * valor1;
-                        Console.WriteLine($"El área del cuadrado es: {area}");
-                        break;
-                    case 2:
-                        Console.WriteLine("Ingrese el valor de la base del rectángulo:");
-                        valor1 = double.Parse(Console.ReadLine());
-                        Console.WriteLine("Ingrese el valor de la altura del rectángulo:");
-                        valor2 = double.Parse(Console.ReadLine());
-                        area = valor1 * valor2;
-                        Console.WriteLine($"El área del rectángulo es: {area}");
-                        break;
-                    case 3:
-                        Console.WriteLine("Ingrese el valor de la base del triángulo:");
-                        valor1 = double.Parse(Console.ReadLine());
-                        Console.WriteLine("Ingrese el valor de la altura del triángulo:");
-                        valor2 = double.Parse(Console.ReadLine());
-                        area = valor1 * valor2 / 2;
-                        Console.WriteLine($"El área del triángulo es: {area}");
-                        break;
-                    case 4:
-                        Console.WriteLine("Ingrese el valor del radio del círculo:");
-                        valor1 = double.Parse(Console.ReadLine());
-                        area = Math.PI * valor1 * valor1;
-                        Console.WriteLine($"El área del círculo es: {area}");
-                        break;
-                    case 0:
-                        Console.WriteLine("Saliendo del programa...");
-                        break;
-                    default:
-                        Console.WriteLine("Opción inválida. Intente de nuevo.");
-                        break;
-                }
-            } while (opcion != 0);
         }
     }
 }
