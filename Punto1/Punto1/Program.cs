eac21d8d8afae911aae3d3b281f243279e4ac2e8
ricardo taborda palacio <richtp@hotmail.com>
2023-03-09 18:43:40 -0500

    pasoprimeros pasos

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -6,7 +6,63 @@ namespace Punto1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcion, shtw = 1;
+            double num1, num2, num3, cuadrado, rectangulo, triangulo, circulo;
+
+
+
+            
+
+
+            Console.WriteLine(" ");
+            Console.WriteLine("                ********************                   ");
+            Console.WriteLine("Punto 1 : Calculadora de Figuras Geometricas");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine(" elige una de las opciones del menu");
+            Console.WriteLine("   1 Para Calcular Area de Cuadrado");
+            Console.WriteLine("   2 Para Calcular Area de Un Rectangulo");
+            Console.WriteLine("   3 Para Calcular Area de Un Triangulo");
+            Console.WriteLine("   4 Para Calcular Area de Un Circulo");
+            Console.WriteLine("   0 Para Salir");
+
+     
+            opcion =Convert.ToInt32(Console.ReadLine()); 
+
+            switch (opcion)
+            {
+                case 0:
+                    shtw=0;
+                
+                    break;
+                    case 1:
+                    Console.WriteLine("Ingresa Por Favor el valor del Lado del Cuadrado");
+                    num1= Convert.ToInt32(Console.ReadLine());
+                    cuadrado = num1 * num1;
+
+                    Console.WriteLine(" ");
+                    Console.WriteLine(cuadrado);
+                    break;
+                case 2:
+                    Console.WriteLine("Ingresa Por Favor el valor de la BASE");
+                    num1= Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Ingresa Por Favor el Valor de la ALTURA");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+                    rectangulo = num1 * num2;
+                    break;
+
+
+
+            
+            
+            
+            }
+
+            
+
+
         }
     }
 }
