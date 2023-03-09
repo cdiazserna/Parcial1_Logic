80eb3193579bc679329dd6bb11ea57d3a161f7f5
Enmanuelle Perez <emanuel17229@gmail.com>
2023-03-09 18:48:27 -0500

    PUNTO1

--- PUNTO1/Program.cs
+++ PUNTO1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace PUNTO1
 {
@@ -9,6 +10,60 @@ namespace PUNTO1
             Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
             Console.WriteLine("PROGRAMA PARA CALCULAR ÁREAS");
             Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+
+            double primernum, segundonum;
+            double cuad, tria, rect, circ;
+            int menu = 1;
+            int resultado;
+
+            while (menu == 1)
+            {
+                Console.WriteLine("Qué operación desea hacer?");
+
+                Console.WriteLine("---------------------------");
+
+                Console.WriteLine("Calcular el área de un cuadrado (PRESIONE 1) ");
+                Console.WriteLine("Calcular el área de un rectangulo (PRESIONE 2)");
+                Console.WriteLine("Calcular el área de un triángulo (PRESIONE 3)");
+                Console.WriteLine("Calcular el área de un círculo (PRESIONE 4)");
+
+                resultado = Convert.ToInt32(Console.ReadLine());
+
+                switch (resultado)
+                {
+                    case 1:
+                        suma = primernumero + segundonumero;
+                        Console.WriteLine("El resultado de la suma es:" + suma);
+                        break;
+
+                    case 2:
+                        resta = primernumero - segundonumero;
+                        Console.WriteLine("El resultado de la resta es:" + resta);
+                        break;
+
+                    case 3:
+                        mult = primernumero * segundonumero;
+                        Console.WriteLine("El resultado de la multiplicación es:" + mult);
+                        break;
+
+                    case 4:
+                        if (segundonumero == 0)
+                        {
+                            Console.WriteLine("La división es indeterminada");
+
+                        }
+                        else
+                        {
+                            division = primernumero / segundonumero;
+                            Console.WriteLine("El resultado de la división es:" + division);
+                        }
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Presione una opción válida");
+                        break;
+                }
 
         }
     }
