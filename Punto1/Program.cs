63d65b13d64f45af01c44d9bb3e5ea3548d81e8c
JAIRO LEANDRO MORA GUERRA <jairomora214411@correo.itm.edu.co>
2023-03-09 19:10:21 -0500

    se crea caso para opcion 1

--- Punto1/Program.cs
+++ Punto1/Program.cs
@@ -6,7 +6,33 @@ namespace Punto1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int ladcuad, ladrect1, ladrect2, basetri, alttri, area, radio,option;
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Calculadora de areas");
+           
+            Console.WriteLine("Digite 1 para cubo");
+            
+            Console.WriteLine("Digite 2 para rectangulo");
+           
+            Console.WriteLine("Digite 3 para triangulo");
+            
+            Console.WriteLine("Digite 4 para circulo");
+
+            option=Convert.ToInt32(Console.ReadLine());
+            
+            
+                switch(option)
+                { 
+                   case 1:
+                    
+                        Console.WriteLine("Digite el lado del cubo:");
+                        Console.WriteLine("el area del cubo es :"+ladcuad*ladcuad); break;
+
+                    
+            }
+
+
         }
     }
 }
