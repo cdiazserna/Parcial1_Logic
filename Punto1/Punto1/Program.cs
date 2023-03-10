e0fdc1b698a0ee10a6a64fd0c829eb0f2c7c8efc
jjtafur <jjtafur22@gmail.com>
2023-03-09 19:29:20 -0500

    Defición de variables

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -7,11 +7,10 @@ namespace Punto1
         static void Main(string[] args)
         {
             int opcion;
-            double valor1, valor2, area;
+            double val1, val2, area;
 
             do
             {
-
                 Console.WriteLine("Seleccione una opción, para calcular el área de la figuras mencionadas:");
                 Console.WriteLine("0. Salir");
                 Console.WriteLine("1. Calcular área de un cuadrado");
@@ -29,30 +28,30 @@ namespace Punto1
                         break;
                     case 1:
                         Console.WriteLine("Ingrese el valor del lado del cuadrado:");
-                        valor1 = Convert.ToDouble(Console.ReadLine());
-                        area = valor1 * valor1;
+                        val1 = Convert.ToDouble(Console.ReadLine());
+                        area = val1 * val1;
                         Console.WriteLine("El área del cuadrado es: " + area);
                         break;
                     case 2:
                         Console.WriteLine("Ingrese el valor de la base del rectángulo:");
-                        valor1 = Convert.ToDouble(Console.ReadLine());
+                        val1 = Convert.ToDouble(Console.ReadLine());
                         Console.WriteLine("Ingrese el valor de la altura del rectángulo:");
-                        valor2 = Convert.ToDouble(Console.ReadLine());
-                        area = valor1 * valor2;
+                        val2 = Convert.ToDouble(Console.ReadLine());
+                        area = val1 * val2;
                         Console.WriteLine("El área del rectángulo es: " + area);
                         break;
                     case 3:
                         Console.WriteLine("Ingrese el valor de la base del triángulo:");
-                        valor1 = Convert.ToDouble(Console.ReadLine());
+                        val1 = Convert.ToDouble(Console.ReadLine());
                         Console.WriteLine("Ingrese el valor de la altura del triángulo:");
-                        valor2 = Convert.ToDouble(Console.ReadLine());
-                        area = valor1 * valor2 / 2;
+                        val2 = Convert.ToDouble(Console.ReadLine());
+                        area = val1 * val2 / 2;
                         Console.WriteLine("El área del triángulo es: " + area);
                         break;
                     case 4:
                         Console.WriteLine("Ingrese el valor del radio del círculo:");
-                        valor1 = Convert.ToDouble(Console.ReadLine());
-                        area = Math.PI * valor1 * valor1;
+                        val1 = Convert.ToDouble(Console.ReadLine());
+                        area = Math.PI * val1 * val1;
                         Console.WriteLine("El área del círculo es: " + area);
                         break;
                     default:
---
+++ Punto2/Punto2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Punto2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cantidad de números que desea evaluar: ");
+            int cantidadNumeros = int.Parse(Console.ReadLine());
+
+            int[] numeros = new int[cantidadNumeros];
+            int suma = 0;
+            int mayor = 0;
+            int menor = int.MaxValue;
+            int cantidadPrimos = 0;
+
+            for (int i = 0; i < cantidadNumeros; i++)
+            {
+                Console.Write("Ingrese el número {0}: ", i + 1);
+                numeros[i] = int.Parse(Console.ReadLine());
+                suma += numeros[i];
+
+                if (numeros[i] > mayor)
+                {
+                    mayor = numeros[i];
+                }
+
+                if (numeros[i] < menor)
+                {
+                    menor = numeros[i];
+                }
+
+                if (esPrimo(numeros[i]))
+                {
+                    cantidadPrimos++;
+                }
+            }
+
+            double promedio = (double)suma / cantidadNumeros;
+
+            Console.WriteLine("El promedio es: {0}", promedio);
+            Console.WriteLine("El mayor número es: {0}", mayor);
+            Console.WriteLine("El menor número es: {0}", menor);
+            Console.WriteLine("La cantidad de números primos es: {0}", cantidadPrimos);
+
+            Console.ReadLine();
+        }
+
+        static bool esPrimo(int numero)
+        {
+            if (numero <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
