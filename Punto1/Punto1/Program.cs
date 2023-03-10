109defd4753e39bfc49f2e48b52db1f4a88e83b6
Andy Morales <andyemmejia@gmail.com>
2023-03-09 19:40:03 -0500

    Commit final

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -9,69 +9,76 @@ namespace Punto1
             double lado, b, altura,radio, areaCuad, areaTrian, areaRectan, areaCircu;
             int option, sw=1;
 
-
-                    Console.WriteLine("******");
-                    Console.WriteLine("*Menú*");
-                    Console.WriteLine("******");
+            while (sw == 1)
+            {
+                    Console.WriteLine("********************************");
+                    Console.WriteLine("**************Menú**************");
+                    Console.WriteLine("********************************");
+                    Console.WriteLine("");
                     Console.WriteLine("1. Calcular área de un cuadrado ");
                     Console.WriteLine("2. Calcular área de un rectángulo");
                     Console.WriteLine("3. Calcular área de un triángulo");
                     Console.WriteLine("4. Calcular área de un círculo");
-                    Console.WriteLine("Digite 0 para salir");
+                    Console.WriteLine("   Digite 0 para salir");
+                    Console.WriteLine("");
 
                     Console.WriteLine("Selecciona una de las opciones del menú");
                     option = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("");
 
-                    Console.WriteLine("Por favor ingrese el lado");
+                    Console.Write("Por favor ingrese el lado: ");
                     lado = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("");
 
-                    Console.WriteLine("Por favor ingrese el lado");
+                    Console.Write("Por favor ingrese la base: ");
                     b = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("");
 
-                    Console.WriteLine("Por favor ingrese el lado");
+                    Console.Write("Por favor ingrese la altura: ");
                     altura = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("");
 
-                    Console.WriteLine("Por favor ingrese el lado");
+                Console.Write("Por favor ingrese el radio: ");
                     radio = Convert.ToDouble(Console.ReadLine());
 
-            while (sw == 1)
-            {
-                   
-                        switch (option)
-                        {
+                     switch (option)
+                     {
                             case 1:
 
                                 areaCuad = Math.Pow(lado, 2);
                                 Console.WriteLine($"El área del cuadrado es: {areaCuad}");
+                                Console.WriteLine("");
                                 break;
 
                             case 2:
 
                                 areaRectan = b * altura;
                                 Console.WriteLine($"El área del rectángulo es: {areaRectan}");
+                                Console.WriteLine("");
                                 break;
 
-                            case 3:
+                    case 3:
 
                                 areaTrian = (b * altura) / 2;
                                 Console.WriteLine($"Elk área del triángulo es: {areaTrian}");
+                                Console.WriteLine("");
                                 break;
 
-                            case 4:
+                    case 4:
 
                                 areaCircu = Math.PI * Math.Pow(radio, 2);
                                 Console.WriteLine($"Elk área del círculo es: {areaCircu}");
+                                Console.WriteLine("");
                                 break;
 
-                        }
+                }
 
+                    Console.WriteLine("Digite cualquier número para volver al menú ó 0 para salir");
+                    sw = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("");
+                    
 
             }
-
-
-
-
-
 
         }
     }
