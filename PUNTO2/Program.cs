2dd9fa12fd56a7aea00c1bbecc764adada4dfd0e
Enmanuelle Perez <emanuel17229@gmail.com>
2023-03-09 20:09:39 -0500

    PUNTO2

--- PUNTO2/Program.cs
+++ PUNTO2/Program.cs
@@ -6,9 +6,9 @@ namespace PUNTO2
     {
         static void Main(string[] args)
         {
-            int numeros;
-            double promedio;
-            int nummayor, nummenor, numprimos;
+            int numeros, NUM,
+            double promediototal;
+            int nummayor, nummenor, numprimos, sumanumero;
             int operadorfor;
 
             Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
@@ -20,8 +20,11 @@ namespace PUNTO2
             Console.WriteLine("Cúantos números desea ingresar");
             numeros = Convert.ToInt32(Console.ReadLine());
 
-            for (operadorfor=1; operadorfor<numeros; operadorfor++)
+            for (operadorfor=0; operadorfor<numeros; operadorfor++)
             {
+                Console.WriteLine("Ingresa los números por favor");
+                NUM=Convert.ToInt32(Console.ReadLine()) ;
+
 
             }
 
