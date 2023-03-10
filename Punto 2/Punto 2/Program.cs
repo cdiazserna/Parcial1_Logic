cbf1c5a834817c98c70241f5780dc07d904ad7fd
Juan Pablo Rebolledo Almeida <rebolledoalmeida@hotmail.com>
2023-03-09 20:34:48 -0500

    Punto 2

---
+++ Punto 2/Punto 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Punto_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algoritmo para calcular el promedio de N numeros");
+
+            Console.WriteLine("cuantos numeros desea ingresar?: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            bool switch1 = true;
+
+            while (switch1)
+            {
+                if (n > 0)
+                {
+                    switch1= false;
+                }
+            }
+
+            while (num > 0)
+            {
+
+            }
+        }
+    }
+}
