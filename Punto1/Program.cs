92d50c16ef60525910c6097b931936c69331a018
JAIRO LEANDRO MORA GUERRA <jairomora214411@correo.itm.edu.co>
2023-03-09 20:15:42 -0500

    Codigo terminado

--- Punto1/Program.cs
+++ Punto1/Program.cs
@@ -5,66 +5,67 @@ namespace Punto1
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            int ladcuad, ladrect1, ladrect2, basetri, alttri, area, radio,option;
-
-            Console.WriteLine("----------------------------------");
-            Console.WriteLine("Calculadora de areas");
-            Console.WriteLine("----------------------------------");
-
-            Console.WriteLine("Digite 1 para cubo");
-            
-            Console.WriteLine("Digite 2 para rectangulo");
-           
-            Console.WriteLine("Digite 3 para triangulo");
-            
-            Console.WriteLine("Digite 4 para circulo");
-
-            option=Convert.ToInt32(Console.ReadLine());
-            
-            
-                switch(option)
-                { 
+            int ladcuad, ladrect1, ladrect2, basetri, alttri, area, radio, option;
+            do
+            {
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("Calculadora de areas");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("Digite 0 para salir");
+                Console.WriteLine("Digite 1 para volver al menu");
+                Console.WriteLine("Digite 2 para evaluar cubo");
+                Console.WriteLine("Digite 3 para evaluar rectangulo");
+                Console.WriteLine("Digite 4 para evaluar triangulo");
+                Console.WriteLine("Digite 5 para evaluar circulo");
+                Console.WriteLine("----------------------------------");
+                
+                option=Convert.ToInt32(Console.ReadLine());
 
 
+                switch (option)
+                {
 
-
-
-
-
-
-                   case 1:
-                    
-                        Console.WriteLine("Digite el lado del cubo:");
-                        ladcuad=Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("el area del cubo es :"+(ladcuad*ladcuad)); break;
+                    case 0:break;
 
                     case 2:
-                    
+
+                        Console.WriteLine("Digite el lado del cubo:");
+                        ladcuad=Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("el area del cubo es :"+(ladcuad*ladcuad)); 
+                        Console.WriteLine("seleccione una nueva opcion"); 
+                        option=Convert.ToInt32(Console.ReadLine());
+                        break;
+
+
+                    case 3:
+
                         Console.WriteLine("Digite el lado 1 del rectangulo:");
                         ladrect1=Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("Digite el lado 2 del rectangulo:");
                         ladrect2=Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("el area del cubo es :"+(ladrect1*ladrect2)); break;
 
-                case 3:
+                    case 4:
 
-                    Console.WriteLine("Digite la base del triangulo:");
-                    basetri=Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Digite el lado 1 del rectangulo:");
-                    alttri=Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("el area del cubo es :"+((basetri*alttri)/2)); break;
+                        Console.WriteLine("Digite la base del triangulo:");
+                        basetri=Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Digite la altura del triangulo:");
+                        alttri=Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("el area del cubo es :"+((basetri*alttri)/2)); break;
 
-                case 4:
+                    case 5:
 
-                    Console.WriteLine("Digite el radio de la circunferencia:");
-                    radio=Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("el area del circulo es :"+(Math.Pow(radio, 2)*(System.Math.PI))); break;
-
-
-            }
+                        Console.WriteLine("Digite el radio de la circunferencia:");
+                        radio=Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("el area del circulo es :"+(Math.Pow(radio, 2)*(System.Math.PI))); break;
 
 
-        }
-    }
+                }
+
+            } while (option==1);
+            
+        }    
+    }   
 }
