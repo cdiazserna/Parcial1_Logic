8db216f8126126d58c9c0e9534d9181e090d5553
Enmanuelle Perez <emanuel17229@gmail.com>
2023-03-09 19:55:22 -0500

    Punto2

--- PUNTO2/Program.cs
+++ PUNTO2/Program.cs
@@ -6,15 +6,24 @@ namespace PUNTO2
     {
         static void Main(string[] args)
         {
-            int numero;
+            int numeros;
             double promedio;
             int nummayor, nummenor, numprimos;
+            int operadorfor;
 
             Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
             Console.WriteLine("OPERACIONES ENTRE N NÚMEROS NATURALES");
             Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
 
+            Console.WriteLine("---------------------------------------------------------------------");
 
+            Console.WriteLine("Cúantos números desea ingresar");
+            numeros = Convert.ToInt32(Console.ReadLine());
+
+            for (operadorfor=1; operadorfor<numeros; operadorfor++)
+            {
+
+            }
 
         }
     }
