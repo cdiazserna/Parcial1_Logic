0a00052a8b7b4251f4ea3f254bff85d7fb27414a
Andy Morales <andyemmejia@gmail.com>
2023-03-09 19:11:21 -0500

    Commit 3

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -7,61 +7,68 @@ namespace Punto1
         static void Main(string[] args)
         {
             double lado, b, altura,radio, areaCuad, areaTrian, areaRectan, areaCircu;
-            int option;
-
-            Console.WriteLine("******");
-            Console.WriteLine("*Menú*");
-            Console.WriteLine("******");
-            Console.WriteLine("1. Calcular área de un cuadrado ");
-            Console.WriteLine("2. Calcular área de un rectángulo");
-            Console.WriteLine("3. Calcular área de un triángulo");
-            Console.WriteLine("4. Calcular área de un círculo");
-
-            option = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Por favor ingrese el lado");
-            lado = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Por favor ingrese el lado");
-            b = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Por favor ingrese el lado");
-            altura = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Por favor ingrese el lado");
-            radio = Convert.ToDouble(Console.ReadLine());
+            int option, sw=1;
 
 
+                    Console.WriteLine("******");
+                    Console.WriteLine("*Menú*");
+                    Console.WriteLine("******");
+                    Console.WriteLine("1. Calcular área de un cuadrado ");
+                    Console.WriteLine("2. Calcular área de un rectángulo");
+                    Console.WriteLine("3. Calcular área de un triángulo");
+                    Console.WriteLine("4. Calcular área de un círculo");
+                    Console.WriteLine("Digite 0 para salir");
 
-            switch (option)
+                    Console.WriteLine("Selecciona una de las opciones del menú");
+                    option = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Por favor ingrese el lado");
+                    lado = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Por favor ingrese el lado");
+                    b = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Por favor ingrese el lado");
+                    altura = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Por favor ingrese el lado");
+                    radio = Convert.ToDouble(Console.ReadLine());
+
+            while (sw == 1)
             {
-                case 1:
+                   
+                        switch (option)
+                        {
+                            case 1:
 
-                    areaCuad = Math.Pow(lado, 2);
-                    Console.WriteLine($"El área del cuadrado es: {areaCuad}");
-                    break;
+                                areaCuad = Math.Pow(lado, 2);
+                                Console.WriteLine($"El área del cuadrado es: {areaCuad}");
+                                break;
 
-                case 2:
+                            case 2:
 
-                    areaRectan = b * altura ;
-                    Console.WriteLine($"El área del rectángulo es: {areaRectan}");
-                    break;
+                                areaRectan = b * altura;
+                                Console.WriteLine($"El área del rectángulo es: {areaRectan}");
+                                break;
 
-                case 3:
+                            case 3:
 
-                    areaTrian = (b * altura) / 2;
-                    Console.WriteLine($"Elk área del triángulo es: {areaTrian}");
-                    break;
+                                areaTrian = (b * altura) / 2;
+                                Console.WriteLine($"Elk área del triángulo es: {areaTrian}");
+                                break;
 
-                case 4:
+                            case 4:
 
-                    areaCircu = Math.PI * Math.Pow(radio,2);
-                    Console.WriteLine($"Elk área del círculo es: {areaCircu}");
-                    break;
+                                areaCircu = Math.PI * Math.Pow(radio, 2);
+                                Console.WriteLine($"Elk área del círculo es: {areaCircu}");
+                                break;
 
+                        }
 
 
             }
+
+
 
 
 
